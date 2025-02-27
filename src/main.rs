7151db36b9e1213ad582a667cc98526293784e06
Jonathan Fischer <jonathan.fischer@sony.com>
2025-02-26 17:26:40 -0800

    Cleaned up handle_keypress; added watch command.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 mod digit_keys;
 mod particle;
 mod sim;
-mod spatial_hash;
 
 use bevy::color::palettes::basic::*;
 use bevy::color::palettes::css::GOLD;
@@ -20,7 +19,7 @@ fn main() {
     App::new()
         .add_plugins((DefaultPlugins, FrameTimeDiagnosticsPlugin))
         .add_systems(Startup, setup)
-        .add_systems(Update, (update, draw_debug_info, handle_keypress, handle_mouse_clicks, on_resize, update_fps))
+        .add_systems(Update, (update_particles, draw_debug_info, handle_keypress, handle_mouse_clicks, on_resize, update_fps))
         .run();
 }
 
@@ -41,7 +40,7 @@ fn setup(
             // Create a Text with multiple child spans.
             Text::new("FPS: "),
             TextFont {
-                font_size: 14.0,
+                font_size: 16.0,
                 ..default()
             },
         ))
@@ -49,7 +48,7 @@ fn setup(
             TextSpan::default(),
             (
                 TextFont {
-                    font_size: 14.0,
+                    font_size: 16.0,
                     ..default()
                 },
                 TextColor(GOLD.into()),
@@ -58,7 +57,7 @@ fn setup(
         ));
 }
 
-fn update(
+fn update_particles(
     mut commands: Commands,
     mut particle_query: Query<(Entity, &mut Transform, &mut Particle)>,
     mut materials: ResMut<Assets<ColorMaterial>>,
@@ -114,18 +113,15 @@ fn update_fps(diagnostics: Res<DiagnosticsStore>, mut query: Query<&mut TextSpan
 fn draw_debug_info(mut gizmos: Gizmos, particle_query: Query<(&Transform, &Particle)>, sim: Single<&Simulation>) {
     if sim.debug.show_arrows {
         particle_query.iter().for_each(|(transform, particle)| {
-            let arrow_end = transform.translation.xy() + sim.velocities[particle.id];
+            let arrow_end = transform.translation.xy() + sim.velocities[particle.id] * 5.0;
             gizmos
                 .arrow(transform.translation.xy().extend(0.0), arrow_end.extend(0.0), YELLOW)
-                .with_tip_length(0.01);
+                .with_tip_length(0.05);
         });
     }
-    if sim.debug.show_circles {
-        for (i, (transform, _)) in particle_query.iter().enumerate() {
-            if (i % 100) == 0 {
-                gizmos.circle_2d(transform.translation.xy(), sim.smoothing_radius, LIME);
-            }
-        }
+    if sim.debug.show_smoothing_radius {
+        let (transform, _) = particle_query.iter().next().unwrap();
+        gizmos.circle_2d(transform.translation.xy(), sim.smoothing_radius, LIME);
     }
 }
 
@@ -133,7 +129,11 @@ fn handle_keypress(
     kb: Res<ButtonInput<KeyCode>>,
     mut app_exit: EventWriter<AppExit>,
     mut sim: Single<&mut Simulation>,
+    camera_query: Single<(&Camera, &GlobalTransform)>,
+    mut particle_query: Query<(&mut Transform, &mut Particle)>,
+    window: Single<&Window>,
 ) {
+    // Space: freeze / unfreeze particle motion.
     if kb.just_pressed(KeyCode::Space) {
         if sim.frames_to_advance() == 0 {
             sim.set_frames_to_show(u32::MAX);
@@ -141,18 +141,22 @@ fn handle_keypress(
             sim.set_frames_to_show(0);
         }
     }
+    // 1-0: advance 1-10 frames.
     if kb.any_just_pressed(DIGIT_KEYS) {
         kb.get_just_pressed().for_each(|key| {
             let count = sim.frames_to_advance() + key_number(key);
             sim.set_frames_to_show(count);
         });
     }
+    // A: toggle velocity arrows
     if kb.just_pressed(KeyCode::KeyA) {
         sim.toggle_arrows();
     }
+    // C: toggle smoothing radius circle.
     if kb.just_pressed(KeyCode::KeyC) {
         sim.toggle_circles();
     }
+    // G: increase/decrease gravity
     if kb.just_pressed(KeyCode::KeyG) {
         if kb.pressed(KeyCode::ShiftLeft) || kb.pressed(KeyCode::ShiftRight) {
             sim.adj_gravity(-0.5);
@@ -160,15 +164,15 @@ fn handle_keypress(
             sim.adj_gravity(0.5);
         }
     }
-    if kb.just_pressed(KeyCode::KeyV) {
-        sim.toggle_inc_velocity();
-    }
+    // L: log debug info in the next frame
     if kb.just_pressed(KeyCode::KeyL) {
         sim.log_next_frame();
     }
+    // R: reset the simulation
     if kb.just_pressed(KeyCode::KeyR) {
         sim.reset();
     }
+    // S: increase/decrease smoothing radius.
     if kb.just_pressed(KeyCode::KeyS) {
         if kb.pressed(KeyCode::ShiftLeft) || kb.pressed(KeyCode::ShiftRight) {
             sim.adj_smoothing_radius(0.01);
@@ -176,7 +180,36 @@ fn handle_keypress(
             sim.adj_smoothing_radius(-0.01);
         }
     }
-    if kb.just_pressed(KeyCode::Escape) {
+    // V: toggle whether velocity is incremented or just assigned (see sim.calculate_pressure()). 
+    if kb.just_pressed(KeyCode::KeyV) {
+        sim.toggle_inc_velocity();
+    }
+    // W: "watch" the particle(s) under the cursor (color them yellow).
+    // Shift-W: clear all watched particles.
+    if kb.just_pressed(KeyCode::KeyW) {
+        if kb.pressed(KeyCode::ShiftLeft) || kb.pressed(KeyCode::ShiftRight) {
+            particle_query.par_iter_mut().for_each(|(_, mut particle)| particle.watched = false);
+        } else {
+            let Some(cursor_position) = window.cursor_position() else {
+                return;
+            };
+            let (camera, camera_transform) = *camera_query;
+
+            // Calculate a world position based on the cursor's position.
+            let Ok(point) = camera.viewport_to_world_2d(camera_transform, cursor_position) else {
+                return;
+            };
+
+            particle_query.par_iter_mut().for_each(|(transform, mut particle)| {
+                if (transform.translation.xy() - point).length() <= sim.particle_size / 2.0 {
+                    println!("Watching particle {} @{:?}", particle.id, transform.translation.xy());
+                    particle.watched = true;
+                }
+            });
+        }
+    }
+    // Esc / Q: quit the app
+    if kb.just_pressed(KeyCode::Escape) || kb.just_pressed(KeyCode::KeyQ) {
         app_exit.send(AppExit::Success);
     }
 }
@@ -184,12 +217,10 @@ fn handle_keypress(
 // Handles clicks on the plane that reposition the object.
 fn handle_mouse_clicks(
     buttons: Res<ButtonInput<MouseButton>>,
-    // windows: Query<&Window, With<PrimaryWindow>>,
-    window: Single<&Window>,
-    // cameras: Query<(&Camera, &GlobalTransform)>,
     camera_query: Single<(&Camera, &GlobalTransform)>,
     mut particle_query: Query<(&mut Transform, &mut Particle)>,
     sim: Single<&Simulation>,
+    window: Single<&Window>,
 ) {
     if !buttons.just_pressed(MouseButton::Left) {
         return;
@@ -204,12 +235,6 @@ fn handle_mouse_clicks(
         return;
     };
 
-    particle_query.par_iter_mut().for_each(|(transform, mut particle)| {
-        if (transform.translation.xy() - point).length() <= sim.particle_size / 2.0 {
-            println!("Watching particle {} @{:?}", particle.id, transform.translation.xy());
-            particle.watched = true;
-        }
-    });
 }
 
 /// This system shows how to respond to a window being resized.
