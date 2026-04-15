f9551cb880357571e4e673761cb6b452dc9f2def
Jonathan A Fischer <jonathan.fischer@sony.com>
2026-04-15 09:18:42 -0700

    More adjustments to the default args.

--- src/args.rs
+++ src/args.rs
@@ -14,7 +14,7 @@ pub struct Args {
     pub num_particles: usize,
     /// Number of particles
     #[cfg(not(debug_assertions))]
-    #[arg(long, default_value = "30000", visible_alias = "num")]
+    #[arg(long, default_value = "25000", visible_alias = "num")]
     pub num_particles: usize,
     /// Smoothing radius, as a multiple of particle size (e.g. 8.0 = 8x particle diameter).
     #[arg(long, default_value = "10.0")]
@@ -25,11 +25,11 @@ pub struct Args {
     /// Speed multiplier
     #[arg(long, default_value = "3.0")]
     pub speed: f32,
-    #[arg(long, default_value = "200000")]
+    #[arg(long, default_value = "250000")]
     pub pressure_multiplier: u32,
     #[arg(long, default_value = "5.0")]
     pub viscosity_strength: f32,
-    #[arg(long, default_value = "0.5")]
+    #[arg(long, default_value = "0.2")]
     pub collision_damping: f32,
     /// Radius of the area-of-affect for mouse clicks, as a factor of particle size.
     #[arg(long, default_value = "50")]
